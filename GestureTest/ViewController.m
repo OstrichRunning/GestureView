0f23fd610c9dc654e3f61b08791a4f541ab8806a
Craig <18080380931@189.cn>
2018-12-25 11:27:04 +0800

    测试 增加子视图初始化形变

--- GestureTest/ViewController.m
+++ GestureTest/ViewController.m
@@ -31,6 +31,14 @@
     actionView.backgroundColor = UIColor.redColor;
     [_gestureView addSubview:actionView];
     
+    
+    /// 初始化 增加 旋转 缩放 view (所有形变会叠加)
+    CRGestureActionView *transformView = [[CRGestureActionView alloc] initWithFrame:CGRectMake(0.0, 60.0, 100.0, 60.0)];
+    transformView.transform = CGAffineTransformRotate(transformView.transform, M_PI_2 * 0.6);
+    transformView.transform = CGAffineTransformScale(transformView.transform, 1.5, 1.5);
+    transformView.backgroundColor = UIColor.grayColor;
+    [_gestureView addSubview:transformView];
+    
     /// copy视图按钮
     UIButton *copyBtn = [[UIButton alloc] initWithFrame:CGRectMake(100.0, 0.0, 60.0, 40.0)];
     [copyBtn setTitle:@"copy" forState:UIControlStateNormal];
